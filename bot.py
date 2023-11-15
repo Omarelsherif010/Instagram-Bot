9499fcbe27db38b40e06b84a5131f12b9b45a1e6
Omar Elsherif <omarelsherif010@gmail.com>
2023-11-15 10:58:34 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,20 +3,68 @@ from instagrapi.types import Usertag
 import time
 
 # Replace 'your_username' and 'your_password' with your Instagram credentials
-username = 'omarelsherif0101'
-password = 'Collegeboard@010#instagram'
+username = 'omarelsherif0102'
+password = 'Collegeboard@010'
 
 # Create an instance of the Client
 client = Client()
 client.login(username, password)
 
-username_list = ["asmaa_elsherif_010", "s_a_i_d___mp"]
+username_list = ["yousefawadalla94"]
 users_id_list = []
 thread_id_list = []
 
-for item in username_list:
-    user_id = client.user_id_from_username(item)
+for user in username_list:
+    user_id = client.user_id_from_username(user)
     users_id_list.append(user_id)
+
+def get_direct_messages_from_users(users_id_list):
+    messages_list = []
+    for user in users_id_list:
+        direct_messages = client.direct_thread_by_participants(user_ids=[user]) 
+        
+        thread_id = direct_messages['thread']['thread_id']
+        thread_id_list.append(thread_id)
+        print(f"Thread ID List: {thread_id_list}")
+
+        message = direct_messages['thread']['items'][0].dict()
+        messages_list.append(message)
+        print(f"messages Received: {messages_list}")
+    return messages_list
+
+def share_message_as_story(): #message_id
+    # Retrieve the media (photo or video) from the direct message
+    # message = client.direct_v2_item(message_id)
+    message = get_direct_messages_from_users(users_id_list)[0]
+
+    media = message.get('media', [])
+
+    # Check if the message has media
+    if media:
+        # Upload the same media to your story
+        for item in media:
+            media_type = item.get('media_type')
+            media_url = item.get('image_versions2', {}).get('candidates', [])[0].get('url') if media_type == 1 else item.get('video_versions', [])[0].get('url') if media_type == 2 else None
+
+            if media_url:
+                if media_type == 1:  # Photo
+                    client.photo_upload(media_url, caption="Shared from direct message")
+                elif media_type == 2:  # Video
+                    # You can use client.video_upload() for videos (requires additional parameters)
+                    pass
+
+# Example usage
+# Replace 'your_message_id' with the actual ID of the direct message you want to share
+# message_id = 'your_message_id'
+share_message_as_story()
+
+# username_list = ["asmaa_elsherif_010", "s_a_i_d___mp"]
+# users_id_list = []
+# thread_id_list = []
+
+# for item in username_list:
+#     user_id = client.user_id_from_username(item)
+#     users_id_list.append(user_id)
 
 # function to get direct messages from list of users
 # def get_direct_messages_from_users(users_id_list):
@@ -35,14 +83,14 @@ for item in username_list:
 # print(get_direct_messages_from_users(users_id_list), thread_id_list)
 
 # Collect thread ids of direct messages
-for user in users_id_list:
-        direct_messages = client.direct_thread_by_participants(user_ids=[user]) 
+# for user in users_id_list:
+#         direct_messages = client.direct_thread_by_participants(user_ids=[user]) 
         
-        thread_id = direct_messages['thread']['thread_id']
-        thread_id_list.append(thread_id)
-thread_details = client.direct_thread(thread_id=thread_id_list[0])
-link_to_post = thread_details['messages']
-print(f"Thread Details: {link_to_post}")
+#         thread_id = direct_messages['thread']['thread_id']
+#         thread_id_list.append(thread_id)
+# thread_details = client.direct_thread(thread_id=thread_id_list[0])
+# link_to_post = thread_details['messages']
+# print(f"Thread Details: {link_to_post}")
 
 # Get user information
 user = client.user_info_by_username(username)
