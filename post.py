94d6692a4bc7ef9d2133c302456e4d1bac8a8133
Omar Elsherif <omarelsherif010@gmail.com>
2023-11-16 16:10:31 +0200

    Update post.py

--- post.py
+++ post.py
@@ -1,19 +1,32 @@
 from instagrapi import Client
 from instagrapi.types import Usertag
+import time
+
+# Replace 'your_username' and 'your_password' with your Instagram credentials
+username = 'omarelsherif0102'
+password = 'Collegeboard@010'
+
+# Create an instance of the Client
+client = Client()
+client.login(username, password)
+
+username_list = ["yousefawadalla94"]
+users_id_list = []
+thread_id_list = []
+
+for user in username_list:
+    user_id = client.user_id_from_username(user)
+    users_id_list.append(user_id)
 
 
-username = 'omarelsherif0101'
-password = 'Collegeboard@010#instagram'
+# Get the media
+media_pk = client.media_pk_from_url(url="https://www.instagram.com/p/CQ9z0wYnLpC/")
+media_caption = client.media_info(media_pk).dict()['caption_text']
+print(media_caption)
 
-# Initialize the client
-cl = Client()
-cl.login(username, password)
-
-# # Get user information
-# user = cl.user_info_by_username(username)
 
 # # Upload the photo
-# media = cl.photo_upload(
+# media = client.photo_upload(
 #     path="linkedin community post.jpeg",
 #     caption="Join GeeksHub",
 #     usertags=[Usertag(user=user, x=0.5, y=0.5)]
