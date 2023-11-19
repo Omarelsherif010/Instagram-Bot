9b961ece546de77885ac77ede4e91c8f07e2477e
Omar Elsherif <omarelsherif010@gmail.com>
2023-11-19 20:59:10 +0200

    Update post.py

--- post.py
+++ post.py
@@ -1,6 +1,7 @@
+import schedule
+import time
 from instagrapi import Client
 from instagrapi.types import Usertag
-import time
 
 # Replace 'your_username' and 'your_password' with your Instagram credentials
 username = 'omarelsherif0102'
@@ -10,73 +11,86 @@ password = 'Collegeboard@010'
 client = Client()
 client.login(username, password)
 
+### Add Usernames to the list :)
 username_list = ["danilgreen","polianskiyartem"]
+
+
 users_id_list = []
 Messages = {'media_link':[],
            'video_media_pk': []}
 
 
 for user in username_list:
-    user_id = client.user_id_from_username(user)
-    users_id_list.append(user_id)
+    user_id_code = client.user_id_from_username(user)
+    users_id_list.append(user_id_code)
 
-for user_id in users_id_list:    
-    direct_messages = client.direct_thread_by_participants(user_ids=[user_id])
-    # print(f'Direct Messages: {direct_messages}\n')
+def get_and_post_messages():
+    for user_id in users_id_list:    
+        direct_messages = client.direct_thread_by_participants(user_ids=[user_id])
+        # print(f'Direct Messages: {direct_messages}\n')
 
-    ### Photo and Album
-    if direct_messages["thread"]["items"][0]["item_type"] == "xma_media_share":
-        print(f"Item Type: {direct_messages['thread']['items'][0]['item_type']}\n")
-        print(f"Media Share: {direct_messages['thread']['items'][0]['xma_media_share']}\n")
+        ### Photo and Album
+        if direct_messages["thread"]["items"][0]["item_type"] == "xma_media_share":
+            print(f"Item Type: {direct_messages['thread']['items'][0]['item_type']}\n")
+            print(f"Media Share: {direct_messages['thread']['items'][0]['xma_media_share']}\n")
 
-        ## Get the media PK, caption, and thumbnail URL
-        media_link = direct_messages['thread']['items'][0]['xma_media_share'][0]['target_url']
-        print(f"Media Link: {media_link}\n")
-        if media_link not in Messages['media_link']:
-            Messages['media_link'].append(media_link)
+            ## Get the media PK, caption, and thumbnail URL
+            media_link = direct_messages['thread']['items'][0]['xma_media_share'][0]['target_url']
+            print(f"Media Link: {media_link}\n")
+            if media_link not in Messages['media_link']:
+                Messages['media_link'].append(media_link)
 
-            media_url = media_link[:40]
-            media_pk = client.media_pk_from_url(url=media_url)
-            media_caption = client.media_info(media_pk).dict()['caption_text']
-            # print(media_caption)
-            
-            media_path = client.album_download(media_pk=media_pk, folder="albums")
-            # print(f'Media Path: {media_path}\n')
+                media_url = media_link[:40]
+                media_pk = client.media_pk_from_url(url=media_url)
+                media_caption = client.media_info(media_pk).dict()['caption_text']
+                # print(media_caption)
+                
+                media_path = client.album_download(media_pk=media_pk, folder="albums")
+                # print(f'Media Path: {media_path}\n')
 
-            # Upload the album and caption as post
-            media = client.album_upload(
-                paths=media_path,
-                caption=media_caption)
-            # print(media)
+                # Upload the album and caption as post
+                media = client.album_upload(
+                    paths=media_path,
+                    caption=media_caption)
+                # print(media)
 
 
-    ### Video Message
-    elif direct_messages["thread"]["items"][0]["item_type"] == "clip":
-        # print(f"Item Type: {direct_messages['thread']['items'][0]['item_type']}\n")
-        # print(f"Clip: {direct_messages['thread']['items'][0]['clip']}\n")
+        ### Video Message
+        elif direct_messages["thread"]["items"][0]["item_type"] == "clip":
+            # print(f"Item Type: {direct_messages['thread']['items'][0]['item_type']}\n")
+            # print(f"Clip: {direct_messages['thread']['items'][0]['clip']}\n")
 
-        video_media_pk = direct_messages['thread']['items'][0]['clip']['clip']['pk']
+            video_media_pk = direct_messages['thread']['items'][0]['clip']['clip']['pk']
 
-        if video_media_pk not in Messages['video_media_pk']:
-            Messages['video_media_pk'].append(video_media_pk)
+            if video_media_pk not in Messages['video_media_pk']:
+                Messages['video_media_pk'].append(video_media_pk)
 
-            media_caption = client.media_info(video_media_pk).dict()['caption_text']
-            # print(media_caption)
+                media_caption = client.media_info(video_media_pk).dict()['caption_text']
+                # print(media_caption)
 
-            video_url = client.media_info(video_media_pk).dict()['video_url']
-            # print(f'Video URL: {video_url}\n')
+                video_url = client.media_info(video_media_pk).dict()['video_url']
+                # print(f'Video URL: {video_url}\n')
 
-            media_path = client.video_download_by_url(url=video_url, folder="videos")
-            # print(f"Media Path: {media_path}\n") 
+                media_path = client.video_download_by_url(url=video_url, folder="videos")
+                # print(f"Media Path: {media_path}\n") 
 
-            # Upload the video and caption as post
-            media = client.video_upload(
-                path=media_path,
-                caption='video message upload test')
-            # print(media)
+                # Upload the video and caption as post
+                media = client.video_upload(
+                    path=media_path,
+                    caption='video message upload test')
+                # print(media)
 
-    sleep_time_in_seconds = 1 * 30 * 60
-    time.sleep(sleep_time_in_seconds)
+        sleep_time_in_seconds = 1 * 30 * 60
+        time.sleep(sleep_time_in_seconds)
+
+### Main Timer to repeat the whole process
+Main_timer_count_in_seconds = (len(users_id_list) * 30 * 60) + (30 * 60)
+schedule.every(Main_timer_count_in_seconds).seconds.do(get_and_post_messages)
+
+
+
+
+
 
 
     # target_url = direct_messages['thread']['items'][0]['xma_media_share'][0]['target_url']
